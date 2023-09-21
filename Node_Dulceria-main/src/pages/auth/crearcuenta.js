e71fe515cab438442b87c463d57a92b4c7a21980
anibal1015 <128444208+anibal1015@users.noreply.github.com>
2023-09-21 00:54:25 -0500

    t

--- Node_Dulceria-main/src/pages/auth/crearcuenta.js
+++ Node_Dulceria-main/src/pages/auth/crearcuenta.js
@@ -155,12 +155,12 @@ const CrearCuenta = () => {
       <div className="hold-transition login-page">
           <div className="login-box">
               <div className="login-logo">
-                  <Link to="#"><b>Pedido</b> </Link>
+                  <Link to="#"><b>Crear</b> cuenta</Link>
               </div>
 
               <div className="card">
                   <div className="card-body login-card-body">
-                      <p className="login-box-msg">ingrese sus datos</p>
+                      <p className="login-box-msg">Bienvenido, ingrese sus credenciales</p>
                       <form onSubmit={onSubmit}>
   <div className="input-group mb-3">
       <input type="text" className="form-control" placeholder="Nombre" id="nombre" name="nombre" value={nombre} onChange={onChange} required />
@@ -207,10 +207,10 @@ const CrearCuenta = () => {
   
   <div className="social-auth-links text-center mb-3">
       <button type="submit" className="btn btn-block btn-primary">
-          Ingresar
+          Crear
       </button>
       <Link to="/" className="btn btn-block btn-danger">
-          Login
+          Regresar 
       </Link>
   </div>
 </form>
